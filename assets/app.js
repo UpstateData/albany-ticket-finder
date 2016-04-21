21a7732af294917bc0528915741214531e85e8e3
Mark Headd <mheadd@gmail.com>
2016-04-21 15:55:08 -0400

    Better error handling.

--- assets/app.js
+++ assets/app.js
@@ -1,5 +1,5 @@
 // Components of API URL to look up parking violations.
-var url_base = 'https://data.albanyny.gov/resource/474r-rd62.json?license_plate_number=[number]&$select=*';
+var url_base = 'https://data.albanyny.gov/resource/jryy-j5wq.json?license_plate_number=';
 
 // Method to make API call.
 function requestJSON(url, callback) {
@@ -7,62 +7,65 @@ $.ajax({
   url: url,
   beforeSend: function() {
     clearContents();
-    $(".working").show();
+    $('.alert-warning').show();
   },
   complete: function(xhr) {
-     $(".working").hide();
+    $('.alert-warning').hide();
     callback.call(null, xhr.responseJSON);
+  },
+  error: function() {
+    $('.alert-danger').show();
   }
 });
 }
 
 // Clear the contents of the results section, about & search field.
 function clearContents() {
-  $("#results").empty();
-  $(".about").hide();
-  $("#plate").val('');
+  $('#results').empty();
+  $('.alert, .about').hide();
+  $('#plate').val('');
 }
 
 $(document).ready(function() {
 
   // Hide the lookup prompt & about section on page load.
-  $('.working, .about').hide();
+  $('.alert, .about').hide();
 
   // Handler for search button
-  $("#search").click(function() {
-    var searchText = $("#plate").val();
-      if(searchText == "") {
-        $(".form-control").addClass("error");
+  $('#search').click(function() {
+    var searchText = $('#plate').val();
+      if(searchText == '') {
+        $('.form-control').addClass('inputerror');
       }
       else {
-        url = url_base.replace('[number]', searchText);
+        url = url_base + searchText;
         requestJSON(url, function(json) {
           if(json.length == 0) {
-            $("#results").append('<p class="info">No tickets found.</p>');
+            $('.alert-success').show();
           }
           else {
-            details = Handlebars.templates.details({ Details : json });
-            $("#results").append(details);  
+            var details = Handlebars.templates.details({ Details : json });
+            $('#results').append(details);  
           }
         });
       }
   });
 
   // User presses enter.
-  $("#plate").keypress(function(event) {
+  $('#plate').keypress(function(event) {
     var keycode = (event.keyCode ? event.keyCode : event.which);
     if(keycode == '13'){
-      $("#search").trigger('click');  
+      $('#search').trigger('click');  
     }
   });
 
   // Handler for focus on text input field.
-  $("#plate").focus(function(){
-    $(this).removeClass("error");
+  $('#plate').focus(function(){
+    $(this).removeClass('inputerror');
   });
 
   // Show the about section.
-  $("#about").click(function() {
+  $('#about').click(function() {
     $('.about').show();
   });
 
